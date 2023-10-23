6108e9fc807cf605ffd38a4a7530d443ac79c6c1
Galih <galihakbar.ga91@gmail.com>
2023-10-23 16:00:27 +0700

    initial commit

---
+++ migrations/1627983516963_create-table-users.js
@@ -0,0 +1,34 @@
+/* eslint-disable camelcase */
+exports.up = (pgm) => {
+  pgm.createTable('users', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    username: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      unique: true,
+    },
+    email: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      unique: true,
+    },
+    password: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    fullname: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    profileImage: {
+      type: 'TEXT',
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('users');
+};
