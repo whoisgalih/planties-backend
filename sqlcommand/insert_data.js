0537113e92cb1d621dadbdcaeefef019f1c4f9fb
Jan Falih Fadhillah <54056113+jfalih@users.noreply.github.com>
2023-10-30 21:58:11 +0700

    Feat sql rpl

--- sqlcommand/insert_data.js
+++ sqlcommand/insert_data.js
@@ -1,4 +1,24 @@
-const query = {
-    text: 'INSERT INTO authentications VALUES ($1)',
+const plantQuery = {
+    text: 'INSERT INTO oxygen VALUES ($1, $2) RETURNING id, oxygen',
+    values: [id, oxygen],
+};
+
+const userQuery = {
+    text: 'INSERT INTO users VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id, oxygen_id, username, email, password, fullname, profileImage',
+    values: [id, oxygen_id, username, email, password, fullname, profileImage]
+}
+
+const authQuery = {
+    text: 'INSERT INTO authentications VALUES ($1) RETURNING token',
     values: [token],
-  };
+};
+
+const gardenQuery = {
+    text: 'INSERT INTO gardens VALUES ($1, $2, $3, $4) RETURNING id, user_id, name, type',
+    values: [id, user_id, name, type],
+};
+
+const reminderQuery = {
+    text: 'INSERT INTO reminders VALUES ($1, $2, $3, $4) RETURNING id, garden_id, name, type',
+    values: [id, garden_id, name, type]
+}
